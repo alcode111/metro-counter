e6fe45c9d9f4aa74225200e1d4dfb844aa5a690c
Ismail <141173099+alcode111@users.noreply.github.com>
2023-11-12 10:55:57 +0100

    Add first lines of javascript code

---
+++ index.js
@@ -0,0 +1,7 @@
+let count = 0
+
+let peopleCount = document.getElementById("people-count")
+let incrementBtn = document.getElementById("increment-btn")
+
+let saveBtn = document.getElementById("save-btn")
+let previousEntries = document.getElementById("previous-entries")
