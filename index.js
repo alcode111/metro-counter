edd5e21bc6ac77ca530f82a93ec46174b7db579b
Ismail <141173099+alcode111@users.noreply.github.com>
2023-11-12 10:57:25 +0100

    Complete the code with the event listeners

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ let incrementBtn = document.getElementById("increment-btn")
 
 let saveBtn = document.getElementById("save-btn")
 let previousEntries = document.getElementById("previous-entries")
+
+
+incrementBtn.addEventListener('click', () => {
+    count += 1
+    peopleCount.textContent = count
+})
+
+saveBtn.addEventListener('click', () => {
+    previousEntries.textContent += count + " - "
+    peopleCount.textContent = 0
+    count = 0
+})
